7948f5d0e765fa46e99d7d279b312e29e9f2ba2a
laughterbos <isaac.ahlquist@gmail.com>
2018-02-21 23:59:09 -0600

    Create UpdateNote.php

---
+++ UpdateNote.php
@@ -0,0 +1,50 @@
+<?php
+
+header("Content-Type: application/json");
+
+$servername = $_POST['servername'];
+$DBusername = $_POST['DBusername'];
+$DBpassword = $_POST['DBpassword'];
+$database = $_POST['database'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+//Create Connection
+$con = new mysqli($servername, $DBusername, $DBpassword, $database);
+
+// Check connection
+if ($con->connect_error) {
+	die("Connection failed: " . $con->connect_error);
+	$NewArray[] = "Connection failed";
+	echo json_encode($NewArray);
+} 
+
+//Sql query
+if (!($stmt = $con->prepare("SELECT UserID FROM user WHERE Username = ? and Password = ?"))) {
+	$prepareArray[] = "Prepare failed";	
+	echo json_encode($prepareArray);
+}
+
+if (!$stmt->bind_param("ss", $username, $password)) {
+	$bindArray[] = "Bind failed";
+	echo json_encode($bindArray);
+}
+
+if (!$stmt->execute()) {
+	$executeArray[] = "Execute array";
+	echo json_encode($executeArray);
+}
+
+//Process results
+$myArray = array();
+$result = $stmt->get_result();
+while($row = $result->fetch_assoc()) {
+	$myArray[] = $row;
+}
+
+echo json_encode($myArray);
+
+// Close connections
+$stmt->close();
+mysqli_close($con);
+?>
